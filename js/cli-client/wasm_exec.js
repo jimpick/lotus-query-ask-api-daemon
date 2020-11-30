5943a5db78c4d83d95cd9e55da7c62f0144ad9f7
Jim Pick <jim@jimpick.com>
2020-11-29 20:02:24 -0800

    Experimenting with webpack

--- js/cli-client/wasm_exec.js
+++ js/cli-client/wasm_exec.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-global.WebSocket = require('websocket').w3cwebsocket;
-
 (() => {
 	// Map multiple JavaScript environments to a single common API,
 	// preferring web standards over Node.js API.
